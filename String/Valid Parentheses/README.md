a3cfc6638f8b2537ce45095eabb8b234a10a2ba1
Abhinav <abhinav.hfs@gmail.com>
2021-09-28 11:11:28 +0530

    added string questions

---
+++ String/Valid Parentheses/README.md
@@ -0,0 +1,40 @@
+# Valid Parentheses
+
+Given a string s containing just the characters `'(', ')', '{', '}', '[' and ']'`, determine if the input string is valid.
+
+*An input string is valid if:*
+
+- Open brackets must be closed by the same type of brackets.
+- Open brackets must be closed in the correct order.
+ 
+
+### Example 1:
+```
+Input: s = "()"
+Output: true
+```
+### Example 2:
+```
+Input: s = "()[]{}"
+Output: true
+```
+### Example 3:
+```
+Input: s = "(]"
+Output: false
+```
+### Example 4:
+```
+Input: s = "([)]"
+Output: false
+```
+### Example 5:
+```
+Input: s = "{[]}"
+Output: true
+```
+
+### Constraints:
+
+- 1 <= s.length <= 104
+- s consists of parentheses only '()[]{}'.
---
+++ String/Valid Parentheses/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        unordered_map<char, char> mp = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+
+        for (char c : s)
+        {
+            if (mp.find(c) != mp.end())
+            {
+                st.push(c);
+            }
+            else if (!st.empty() && mp[st.top()] == c)
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << s.isValid("()") << endl;
+    cout << s.isValid("()[]{}") << endl;
+    cout << s.isValid("(]") << endl;
+    cout << s.isValid("([)]") << endl;
+    cout << s.isValid("{[]}") << endl;
+    cout << s.isValid("") << endl;
+    cout << s.isValid("(") << endl;
+    cout << s.isValid("[") << endl;
+    cout << s.isValid("{") << endl;
+    return 0;
+}
