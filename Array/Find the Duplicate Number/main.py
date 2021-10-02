d2dae4f8c076a0a02ec76ca6f26ccd0ec416e996
Likhit Ajeesh <76266365+Likkiii@users.noreply.github.com>
2021-10-02 16:33:00 +0400

    Replaced with enumerate() method

--- Array/Find the Duplicate Number/main.py
+++ Array/Find the Duplicate Number/main.py
@@ -1,15 +1,16 @@
+
 # LET's take the first test case
-arr = [1, 3, 3, 2]
+arr=[1,3,3,2]
 
 
-# Intuition
-
+                                                                                            # Intuition
+                                                                                            
 
 # There are n + 1 positive numbers in the array (nums) (all in the range [1, n]). Since the array only contains positive integers, we can track each number (num) that has been seen before by flipping the sign of the number located at index |num|, where || denotes absolute value.
 
 # For example, if the input array is [1, 3, 3, 2], then for 1, flip the number at index 1, making the array [1,−3,3,2]. Next, for −3 flip the number at index 3, making the array [1,−3,3,−2]. Finally, when we reach the second 3, we'll notice that nums[3] is already negative, indicating that 3 has been seen before and hence is the duplicate number.
 
-# Algorithm
+                                                                                            # Algorithm
 
 # Iterate over the array, evaluating each element (let's call the current element curcur).
 
