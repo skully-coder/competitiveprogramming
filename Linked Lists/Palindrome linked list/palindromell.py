893a98a8d59ccb261a32422dd07d226a9b563d36
suvanbalu <suvanbalu@gmail.com>
2021-10-02 20:25:58 +0530

    removed keyword next

--- Linked Lists/Palindrome linked list/palindromell.py
+++ Linked Lists/Palindrome linked list/palindromell.py
@@ -1,25 +1,25 @@
 class LLNode:
-    def __init__(self, val, next=None):
+    def __init__(self, val, link=None):
         self.val = val
-        self.next = next
+        self.link = link
 
 def pallindrome(node):
     fast, slow = node, node
-    while fast and fast.next:
-        fast = fast.next.next
-        slow = slow.next   #middle element finding
+    while fast and fast.link:
+        fast = fast.link.link
+        slow = slow.link   #middle element finding
     right = None
     while slow:
-        p = slow.next     #reversing second half
-        slow.next = right
+        p = slow.link     #reversing second half
+        slow.link = right
         right = slow
         slow = p
     left = node
     while left is not None and right is not None:
         if left.val != right.val:
             return False
-        left = left.next
-        right = right.next
+        left = left.link
+        right = right.link
     return True
 
 n=list(map(int,input().split()))
@@ -30,7 +30,7 @@ for ele in n:
     if head == None:
         head = nnode
     else:
-        p.next = nnode
+        p.link = nnode
     p = nnode
 
 if pallindrome(head) :
