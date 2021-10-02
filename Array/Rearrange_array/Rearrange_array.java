40a0a71b2561b90b2166eabaab47bce3a13db97a
Kunal Jadhav <54142448+jadhav-kunal@users.noreply.github.com>
2021-10-02 23:41:23 +0530

    Name of the class fixed

--- Array/Rearrange_array/Rearrange_array.java
+++ Array/Rearrange_array/Rearrange_array.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class rearrange_array {
+public class Rearrange_array {
     public static void main(String args[])throws IOException
     {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
