d16cdc3b87c1d6681eb2867a88f5d51f5de749fd
aksharshah <akshau0123@gmail.com>
2021-10-03 12:09:37 +0530

    small changes

--- Dynamic Programming/LCSdp/LCS.java
+++ Dynamic Programming/LCSdp/LCS.java
@@ -17,20 +17,19 @@ public class LCS {
         }
         return L[m][n];
     }
-  
 
     public static void main(String[] args)
     {
         LCS lcs = new LCS();
         String s1 = "AGGTAB";
         String s2 = "GXTXAYB";
-  
+
         char[] X = s1.toCharArray();
         char[] Y = s2.toCharArray();
         int m = X.length;
         int n = Y.length;
-  
+
         System.out.println("Length of LCS is"
-                           + " " + lcs.lcs(X, Y, m, n));
+                            + " " + lcs.lcs(X, Y, m, n));
     }
 }
