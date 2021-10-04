a412f5b3e4abe967743a7224b463dcac756ddebf
Pranay Kumar <65260061+pranay40@users.noreply.github.com>
2021-10-05 01:25:36 +0530

    Update Perfectcube.cpp

--- Math/PerfectCube/Perfectcube.cpp
+++ Math/PerfectCube/Perfectcube.cpp
@@ -3,20 +3,15 @@ using namespace std;
 bool Perfectcube(int x)
 {
     float temp = pow(x, 1 / 3.0);
-    return (ceil(temp) == floor(x)) ? true : false;
+    return (pow(ceil(temp),3) == floor(x)) ? true : false;
 }
 
 int main()
 {
     int x;
     cin >> x;
-    if (Perfectcube(x))
-    {
-        cout << "Perfectcube" << endl;
-    }
-    else
-    {
-        cout << "No perfectcube" << endl;
-    }
+    if (Perfectcube(x))   cout << "Perfectcube" << endl;
+    else     cout << "No perfectcube" << endl;
+    
     return 0;
 }
