0886a4b7a60008de56d77882aaaba7a92abb0877
Nanthakumaran S <nanthankumaransenthil@gmail.com>
2021-10-04 11:21:11 +0530

    added Readme.md

--- Trie/AutoCompletion/AutoCompletion.py
+++ Trie/AutoCompletion/AutoCompletion.py
@@ -28,26 +28,6 @@ class Trie:
                 continue
             self._dfsHelper(node[letter], words)
         return words
-
-# Alternative Approach Optimal one
-# O(np) time | O(n) space
-# n = number of elements in array, p = length of prefix, s = length of the longest string in strings
-class OptimalSolution:
-    def autoComplete(self, strings, prefix):
-        result = []
-        for string in strings:
-            left = 0
-            right = len(prefix) - 1
-            add = True
-            while left <= right:
-                if string[left] != prefix[left] or string[right] != prefix[right]:
-                    add = False
-                    break
-                left += 1
-                right -= 1
-            if add:
-                result.append(string)
-        return result
                 
 
 # O(ns + p + e) time | O(ns) space
@@ -64,6 +44,3 @@ if __name__ == "__main__":
         trie.prepareTrie(string)
     
     print(trie.autoComplete("Ha"))
-
-    s = OptimalSolution()
-    print(s.autoComplete(strings, "H"))
