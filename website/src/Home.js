f9c43c73e010d1c9aa35b596a4d853862fe85a76
Abhinav Agrawal <abhinav.hfs@gmail.com>
2021-10-04 10:13:09 +0530

    modified API link becuase of change in repo name

--- website/src/Home.js
+++ website/src/Home.js
@@ -5,10 +5,10 @@ export default function Home() {
 			<div className="row row-eq-spacing">
 				<div className="col-12 col-md-6">
 					<div className="card shadow-lg text-center">
-						<h4>This Website Contains all <b>Contents</b>, <b>Issues</b> of our <a target="_blank" rel="noreferrer" href="https://github.com/skully-coder/competitiveprogramming">repository</a>.</h4>
+						<h4>This Website Contains all <b>Contents</b>, <b>Issues</b> of our <a target="_blank" rel="noreferrer" href="https://github.com/skully-coder/hacktoberfest-competitiveprogramming">repository</a>.</h4>
 						<p style={{ fontSize: "1.5rem"}}>This repository contains <b>solutions</b> and <b>explanations</b> to various <b>competitive programming problems</b>, which are important for <b>interviews</b> and <b>online tests</b> of different companies.</p>
 						<h4><u>Stargazers over time</u>:</h4>
-						<img src="https://starchart.cc/skully-coder/competitiveprogramming.svg" width="100%" alt="Stargazers over time" />
+						<img src="https://starchart.cc/skully-coder/hacktoberfest-competitiveprogramming.svg" width="100%" alt="Stargazers over time" />
 					</div>
 				</div>
 				<div className="col-12 col-md-6 mt-20 mt-md-0">
@@ -20,10 +20,10 @@ export default function Home() {
 								<li><b>Comment</b> on the Issue and make sure it gets <b>Assigned</b> to you.</li>
 								<li><b>Fork</b> this repository in order to add its copy to your repositories.</li>
 								<li><b>Clone</b> your forked repository<br />
-									&emsp;<code className="bg-light-lm bg-dark-light-dm text-center p-5 rounded">git clone https://github.com/&lt;your-username&gt;/competitiveprogramming</code>
+									&emsp;<code className="bg-light-lm bg-dark-light-dm text-center p-5 rounded">git clone https://github.com/&lt;your-username&gt;/hacktoberfest-competitiveprogramming</code>
 								</li>
 								<li>In the cloned repository, <b>Create</b> a folder with the problem name, under the <b>most suitable topic</b>.<br />
-									&emsp;Eg. <code className="bg-light-lm bg-dark-light-dm text-center p-5 rounded">competitiveprogramming / <span className="text-danger">BackTracking</span> / <span className="text-primary font-weight-bold">N-Queens</span></code>
+									&emsp;Eg. <code className="bg-light-lm bg-dark-light-dm text-center p-5 rounded">hacktoberfest-competitiveprogramming / <span className="text-danger">BackTracking</span> / <span className="text-primary font-weight-bold">N-Queens</span></code>
 								</li>
 								<li>In the new folder, please <b>create</b> the following files:
 									<ol style={{ listStyleType: "lower-alpha" }}>
