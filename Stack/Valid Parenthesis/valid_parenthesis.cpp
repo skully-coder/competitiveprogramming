5cae4bea740d83cbd64f35e4007c6a61d4af3b08
bishalpandit <bishalpandit17@gmail.com>
2021-10-07 12:34:03 +0530

    Valid Parenthesis

---
+++ Stack/Valid Parenthesis/valid_parenthesis.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+
+        stack<int> stk;
+        for (auto ch : s)
+        {
+            if (ch == '(' or ch == '[' or ch == '{')
+                stk.push(ch);
+
+            if (ch == ')')
+            {
+                if (stk.empty())
+                    return false;
+                else
+                {
+                    if (stk.top() != '(')
+                        return false;
+                    else
+                        stk.pop();
+                }
+            }
+
+            if (ch == ']')
+            {
+                if (stk.empty())
+                    return false;
+                else
+                {
+                    if (stk.top() != '[')
+                        return false;
+                    else
+                        stk.pop();
+                }
+            }
+
+            if (ch == '}')
+            {
+                if (stk.empty())
+                    return false;
+                else
+                {
+                    if (stk.top() != '{')
+                        return false;
+                    else
+                        stk.pop();
+                }
+            }
+        }
+
+        return stk.empty();
+    }
+};
+
+int main()
+{
+    string s;
+    cin >> s;
+    Solution *obj = new Solution;
+    bool valid = obj->isValid(s);
+    if (valid)
+        cout << "Valid" << '\n';
+    else
+        cout << "Invalid" << '\n';
+}
