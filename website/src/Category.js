93620eeebe6e98347ba7930258d1814084502b73
Abhinav Agrawal <abhinav.hfs@gmail.com>
2022-05-30 14:46:09 +0530

    deployed website

--- website/src/Category.js
+++ website/src/Category.js
@@ -49,53 +49,43 @@ export default function Category(props) {
                 setError(err.response)
             })
     }, [category.url, setError])
-    return ( < div className = "col-12 col-md-10" >
-            <
-            div className = "container text-center" >
-            <
-            h2 className = "m-5 m-md-20" > {
-                category.name
-            } < /h2>  <
-            div className = "table-responsive overflow-auto shadow-lg mx-auto"
-            style = {
-                {
-                    maxHeight: "65vh",
-                    width: "90%"
-                }
-            } >
-            <
-            table className = "table table-bordered table-hover" >
-            <
-            thead className = "position-sticky top-0 bg-dark-lm bg-light-dm text-white-lm text-dark-dm" >
-            <
-            tr >
-            <
-            th > Problem < /th> <th > View Code < /th > < th > View README < /th> <th > Visit Github < /th > < /tr > < /
-            thead > <
-            tbody > {
-                Object.entries(problems).map(([title, data], index) => < Problem key = {
-                        index
-                    }
-                    title = {
-                        title
-                    }
-                    data = {
-                        data
-                    }
-                    category = {
-                        category.name
-                    }
-                    showCode = {
-                        showCode
-                    }
-                    />)} < /
-                    tbody > <
-                    /table> < /
-                    div > <
-                    /div> < /
-                    div >
-                )
-            }
+    return (
+        <div className="col-12 col-md-10">
+  <div className="container text-center">
+    <h2 className="m-5 m-md-20"> {category.name} </h2>{" "}
+    <div
+      className="table-responsive overflow-auto shadow-lg mx-auto"
+      style={{
+        maxHeight: "65vh",
+        width: "90%",
+      }}
+    >
+      <table className="table table-bordered table-hover">
+        <thead className="position-sticky top-0 bg-dark-lm bg-light-dm text-white-lm text-dark-dm">
+          <tr>
+            <th> Problem </th> <th> View Code </th> <th> View README </th>{" "}
+            <th> Visit Github </th>{" "}
+          </tr>{" "}
+        </thead>{" "}
+        <tbody>
+          {" "}
+          {Object.entries(problems).map(([title, data], index) => (
+            <Problem
+              key={index}
+              title={title}
+              data={data}
+              category={category.name}
+              showCode={showCode}
+            />
+          ))}{" "}
+        </tbody>{" "}
+      </table>{" "}
+    </div>{" "}
+  </div>{" "}
+</div>
+
+    )
+            
 
             function Problem(props) {
                 const icons = (language) => {
@@ -107,22 +97,17 @@ export default function Category(props) {
                             />
                         case "cpp":
                             return <span >
-                                <
-                                FontAwesomeIcon icon = {
+                                <FontAwesomeIcon icon = {
                                     faCuttlefish
-                                }
-                            /> <
-                            FontAwesomeIcon icon = {
+                                }/> <FontAwesomeIcon icon = {
                                 faPlus
                             }
                             size = "xs" / >
-                                <
-                                FontAwesomeIcon icon = {
+                                <FontAwesomeIcon icon = {
                                     faPlus
                                 }
                             size = "xs" / >
-                                <
-                                /span>
+                                </span>
                         case "java":
                             return <FontAwesomeIcon className = "font-size-22"
                             icon = {
@@ -141,66 +126,59 @@ export default function Category(props) {
                                 faJsSquare
                             }
                             />
+                        case "exe":
+                            return <></>
                         default:
                             return <span > {
                                 language
-                            } < /span>
+                            } </span>
                     }
                 }
-                return ( <
-                    tr >
-                    <
-                    td > {
+                return ( <tr>
+                    <td> {
                         props.title
-                    } < /td> <
-                    td > {
-                        Object.keys(props.data).map((value, index) => value === "readme" ? null :
-                            <
-                            button key = {
+                    } </td> 
+                    <td> 
+                        {
+                            Object.keys(props.data).map((value, index) => value === "readme" ? null :
+                            <button key = {
                                 index
-                            }
-                            data - toggle = "modal"
-                            data - target = "source-code"
+                            } data-toggle = "modal"
+                            data-target = "source-code"
                             className = "btn btn-success font-weight-semi-bold mr-5 px-10"
                             onClick = {
                                 () => props.showCode(props.data[value], true)
                             } > {
                                 icons(value)
-                            } <
-                            /button>
+                            } </button>
                         )
-                    } <
-                    /td> <
-                    td >
-                    <
-                    button className = "btn btn-primary"
+                    } </td> 
+                    <td >
+                    <button className = "btn btn-primary"
                     onClick = {
                         () => props.showCode(props.data.readme, false)
                     }
-                    data - toggle = "modal"
-                    data - target = "readme" >
-                    <
-                    FontAwesomeIcon icon = {
+                    data-toggle = "modal"
+                    data-target = "readme" >
+                    <FontAwesomeIcon icon = {
                         faFileAlt
                     }
-                    /> < /
-                    button > <
-                    /td> <
-                    td >
-                    <
-                    a target = "_blank"
+                    /> </button > 
+                    </td> 
+                    <td >
+                    <a target = "_blank"
                     rel = "noreferrer"
                     className = "btn btn-secondary"
                     href = {
                         "https://github.com/skully-coder/competitiveprogramming/tree/main/" + props.category + "/" + props.title
                     } >
-                    <
-                    FontAwesomeIcon icon = {
+                    <FontAwesomeIcon icon = {
                         faExternalLinkAlt
                     }
-                    /> < /
-                    a > <
-                    /td> < /
-                    tr >
+                    /> 
+                    </a> 
+                    </td> 
+                    </tr>
                 )
+                }
             }
