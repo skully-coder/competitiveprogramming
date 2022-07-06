b9c8851853ba91747cf148221645fa0d665d4eff
Abhinav Agrawal <55917548+skully-coder@users.noreply.github.com>
2022-07-06 15:21:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (18.24%) - LeetHub

--- Dynamic Programming/509-Fibonacci-Number/509-Fibonacci-Number.cpp
+++ Dynamic Programming/509-Fibonacci-Number/509-Fibonacci-Number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        vector<int> F(50);
+        F[0] = 0;
+        F[1] = 1;
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        for(int i = 2; i<=n; i++)
+        {
+            F[i] = F[i-1] + F[i-2];
+        }
+        return F[n];
     }
 };
