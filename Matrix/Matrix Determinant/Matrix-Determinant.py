fec4d5f43a7301d54480d89f386e16f4774dc3f5
Dhruv Jain <56195235+dhruvjain896@users.noreply.github.com>
2022-10-02 18:54:04 +0530

    Renamed ambiguous variable name l to matrix_size (#237)
    * Renamed ambiguous variable name l to matrix_size
    
    * Renamed ambiguous variable name l to matrix_size

--- Matrix/Matrix Determinant/Matrix-Determinant.py
+++ Matrix/Matrix Determinant/Matrix-Determinant.py
@@ -1,20 +1,20 @@
 def determinant(matrix): #Optimized solution
     #your code here
     result = 0
-    l = len(matrix)
+    matrix_size = len(matrix)
 
     #base case when length of matrix is 1
-    if l == 1:
+    if matrix_size == 1:
         return matrix[0][0]
 
     #base case when length of matrix is 2
-    if l == 2:
+    if matrix_size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
     #for length of matrix > 2
-    for j in range(0, l):
+    for j in range(0, matrix_size):
         # create a sub matrix to find the determinant
-        if l!=2:
+        if matrix_size!=2:
             sub_matrix = []               
             sub_matrix = [(row[0:j]+row[j+1:]) for row in matrix[1:]]
         result = result + (-1)**j * matrix[0][j] * determinant(sub_matrix)
